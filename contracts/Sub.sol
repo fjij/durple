526468b992cf883fcea40615ec99d3c0d9e8cd72
Benjamin Wen <ben@Benjamins-MacBook-Air.local>
2020-10-15 17:05:21 -0400

    Made and tested upDurps and downDurps

--- contracts/Sub.sol
+++ contracts/Sub.sol
@@ -8,14 +8,17 @@ contract Sub {
     struct Post {
       string ipfsHash;
       address op;
+      uint256 udurp;
+      uint256 ddurp;
+      mapping(address => bool) udurped;
+      mapping(address => bool) ddurped;
     }
 
 
     string public version = "v1";
     string public name = "funny";
     Post[] public posts;
-    int public udurp = 0;
-    int public ddurp = 0;
+
 
     address public owner;
 
@@ -27,7 +30,9 @@ contract Sub {
       posts.push(Post(
         {
           ipfsHash: ipfsHash,
-          op: msg.sender
+          op: msg.sender,
+          udurp: 0,
+          ddurp: 0
         }
       ));
     }
@@ -36,15 +41,28 @@ contract Sub {
       return posts.length;
     }
 
-    function getPost(uint256 index) external view returns (string memory, address) {
-      return (posts[index].ipfsHash, posts[index].op);
+    function getPost(uint256 index) external view returns (string memory, address, uint256, uint256) {
+      return (posts[index].ipfsHash, posts[index].op, posts[index].udurp, posts[index].ddurp);
     }
 
-    function upDurp() external {
-      udurp += 1;
+    function upDurp(uint256 index) external {
+      require(!posts[index].udurped[msg.sender], "Already upDurped");
+      if (posts[index].ddurped[msg.sender]) {
+        posts[index].ddurp -= 1;
+        posts[index].ddurped[msg.sender] = false;
+      }
+      posts[index].udurp += 1;
+      posts[index].udurped[msg.sender] = true;
     }
 
-    function downDurp() external {
-      ddurp -= 1;
+    function downDurp(uint256 index) external {
+      require(!posts[index].ddurped[msg.sender], "Already downDurped");
+      if (posts[index].udurped[msg.sender]) {
+        posts[index].udurp -= 1;
+        posts[index].udurped[msg.sender] = false;
+      }
+      posts[index].ddurp += 1;
+      posts[index].ddurped[msg.sender] = true;
     }
+
 }
--- test/Sub.js
+++ test/Sub.js
@@ -34,15 +34,77 @@ describe("Sub contract", function () {
 
     it("Should give us the right hash back", async function() {
       await contract.makePost("hash");
-      let [ipfsHash, op] = await contract.getPost(0);
+      let [ipfsHash, op, ud, dd] = await contract.getPost(0);
       expect(ipfsHash).to.equal("hash");
     });
 
     it("Should have the right op", async function() {
       await contract.makePost("hash");
-      let [ipfsHash, op] = await contract.getPost(0);
+      let [ipfsHash, op, ud, dd] = await contract.getPost(0);
       expect(op).to.equal(await owner.getAddress());
     });
 
   });
+
+  describe("UpDurps", function() {
+
+    it("Should start with zero upDurps", async function() {
+      await contract.makePost("hash");
+      let [ipfsHash, op, ud, dd] = await contract.getPost(0);
+      expect(ud).to.equal(0);
+    });
+
+    it("Should start with zero downDurps", async function() {
+      await contract.makePost("hash");
+      let [ipfsHash, op, ud, dd] = await contract.getPost(0);
+      expect(dd).to.equal(0);
+    });
+
+    it("Should get one upDurp", async function() {
+      await contract.makePost("hash");
+      await contract.upDurp(0);
+      let [ipfsHash, op, ud, dd] = await contract.getPost(0);
+      expect(ud).to.equal(1);
+    });
+
+    it("Should get one downDurp", async function() {
+      await contract.makePost("hash");
+      await contract.downDurp(0);
+      let [ipfsHash, op, ud, dd] = await contract.getPost(0);
+      expect(dd).to.equal(1);
+    });
+
+    it("Should get one downDurp after downDurping after an upDurp", async function() {
+      await contract.makePost("hash");
+      await contract.upDurp(0);
+      await contract.downDurp(0);
+      let [ipfsHash, op, ud, dd] = await contract.getPost(0);
+      expect(dd).to.equal(1);
+    });
+
+    it("Should have no upDurps after downDurping after an upDurp", async function() {
+      await contract.makePost("hash");
+      await contract.upDurp(0);
+      await contract.downDurp(0);
+      let [ipfsHash, op, ud, dd] = await contract.getPost(0);
+      expect(ud).to.equal(0);
+    });
+
+    it("Should have no downDurps after upDurping after a downDurp", async function() {
+      await contract.makePost("hash");
+      await contract.downDurp(0);
+      await contract.upDurp(0);
+      let [ipfsHash, op, ud, dd] = await contract.getPost(0);
+      expect(dd).to.equal(0);
+    });
+    it("Should have one upDurp after upDurping after a downDurp", async function() {
+      await contract.makePost("hash");
+      await contract.downDurp(0);
+      await contract.upDurp(0);
+      let [ipfsHash, op, ud, dd] = await contract.getPost(0);
+      expect(ud).to.equal(1);
+    });
+
+  });
+
 });
