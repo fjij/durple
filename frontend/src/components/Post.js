0e84176a2a365d360ef08dee15e9ec0c4d47830e
J Chan <jchan@js-macbook-pro.lan>
2020-10-16 14:26:23 -0400

    post created

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -3,9 +3,30 @@ import '../styles/style.css';
 
 export function Post() {
   return(
-    <div className="input-group mb-2">
-      <div className="input-group-prepend">
-      </div>
+    <>
+    <form className="mt-4">
+    <div className="form-group">
+      <label for="subDurple">SubDurple</label>
+      <input type="text" className="form-control" id="subDurple"></input>
     </div>
+
+    <div className="form-group">
+      <label for="title">Title</label>
+      <input type="text" className="form-control" id="title"></input>
+    </div>
+
+    <div className="form-group">
+      <label for="image">Image Link (url-link)</label>
+      <input type="url" className="form-control" id="image"></input>
+    </div>
+
+    <div className="form-group">
+      <label for="content">Content</label>
+      <textarea className="form-control" id="content" rows="5"></textarea>
+    </div>
+
+    <input class="btn btn-primary" type="submit" value="Submit"></input>
+    </form>
+    </>
   )
 }
