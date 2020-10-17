eab84d9cfc240990917c7c515ded323b96b0524a
kaStinju <40683050+kaStinju@users.noreply.github.com>
2020-10-16 22:18:16 -0400

    Update SingleCard.js
    small uselss edit

--- frontend/src/components/SingleCard.js
+++ frontend/src/components/SingleCard.js
@@ -45,6 +45,7 @@ export function SingleCard({contentId}) {
           <div className="d-flex flex-row p-0">
               <Voter contentId={contentId}/>
               <CommentWidget contentId={contentId}/>
+              {post?post.hotness:""}
           </div>
         </div>
       </div>
