58f771757bb608a8a82fe652e0b3901b9c266a0e
BuildTools <unconfigured@null.spigotmc.org>
2020-10-17 15:30:06 -0400

    dir

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -4,14 +4,14 @@ import logo from '../assets/logo.png'
 export function About() {
   return (
   <>
-  <dir class="container-fluid">
-  <dir className="aboutCenter">
+  <div className="container-fluid">
+  <div className="aboutCenter">
     <img src={logo}></img>
     <h1>The Future of Forums</h1>
     <h5>Durple is the future of the internet. Updurp or Downdurp. </h5>
     <h5>Uplurkle or Lurkle. Join now and be a Durple.</h5>
-  </dir>
-  </dir>
+  </div>
+  </div>
   </>
 );
 }
