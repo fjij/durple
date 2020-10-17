b344d5acd7eaf2fbdd1ea47070acc317d450f6c5
BuildTools <unconfigured@null.spigotmc.org>
2020-10-17 16:14:38 -0400

    Make post page show subdurple

--- frontend/src/components/MakePost.js
+++ frontend/src/components/MakePost.js
@@ -1,13 +1,12 @@
 import React,{useState} from "react";
 import {Redirect} from "react-router-dom";
 import '../styles/style.css';
-import { useDurpleContext, useSubAddress } from '../hooks/Durple';
+import { useDurpleContext, useSubAddress, useSubData } from '../hooks/Durple';
 import { useParams } from 'react-router-dom';
 
 export function MakePost() {
   const[redirect, setRedirect] = useState(false)
   const durple = useDurpleContext();
-  const[subDurple, setSubDurple] = useState("")
   const[title, setTitle] = useState("")
   const[image, setImage] = useState("")
   const[content, setContent] = useState("")
@@ -16,6 +15,10 @@ export function MakePost() {
   const { subAddress } = useParams();
 
   useSubAddress();
+
+  const subData = useSubData();
+  let subname = subAddress;
+  if (subData) subname = subData.name;
 
   if (redirect) {
     return <Redirect to={"/d/"+subAddress}/>
@@ -28,13 +31,8 @@ export function MakePost() {
       setRedirect(true)
     }}>
     <div className="container">
-    <div className="form-group">
-      <label htmlFor="subDurple">SubDurple</label>
-      <input type="text" className="form-control" id="subDurple" value={subDurple} onChange={(e)=>{
-        setSubDurple(e.target.value)
-      }}></input>
-    </div>
 
+    <h3 className="mb-3">Post to d/{subname}</h3>
     <div className="form-group">
       <label htmlFor="title">Title</label>
       <input type="text" className="form-control" id="title" value={title} onChange={(e)=>{
