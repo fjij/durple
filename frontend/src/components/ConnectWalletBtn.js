ede87924ed5f23b16ac3d37ec43e54087b01f536
BuildTools <unconfigured@null.spigotmc.org>
2020-10-17 14:57:51 -0400

    Auto connect wallet if they have previously pressed the connect btn

--- frontend/src/components/ConnectWalletBtn.js
+++ frontend/src/components/ConnectWalletBtn.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
 import '../styles/style.css';
 import { useDurpleContext } from '../hooks/Durple';
 
 export function ConnectWalletBtn() {
   const durple = useDurpleContext();
-  if (durple.selectedAddress) {
-    return(
-       <>
-       <div className="nav-item nav-link disabled">
-      Connected
-      </div>
-      </>
-    )
+  const localStorage = window.localStorage;
+  const shouldAutoConnect = localStorage.getItem("shouldAutoConnect");
+
+  useEffect(() => {
+    if (shouldAutoConnect) {
+      durple.connectWallet();
+    }
+  }, [shouldAutoConnect]);
+
+  function connect() {
+    durple.connectWallet();
+    localStorage.setItem('shouldAutoConnect', true);
   }
-  return(
-      <button className="btn btn-warning btn-sm" onClick={durple.connectWallet}>Connect Wallet</button>
-  )
+
+  if (window.ethereum === undefined) {
+    return ( <div className="nav-item nav-link disabled"> No Wallet Detected </div> );
+  } else if (durple.selectedAddress) {
+    return ( <div className="nav-item nav-link disabled"> Connected </div> );
+  } else if (shouldAutoConnect) {
+    return ( <div className="nav-item nav-link disabled"> Connecting... </div> );
+  } else {
+    return ( <button className="btn btn-warning btn-sm" onClick={connect}>Connect Wallet</button> );
+  }
 }
