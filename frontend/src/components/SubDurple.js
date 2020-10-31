7717e1fa62a484560e954492b7ffafd09b7e7523
fjij <30779570+fjij@users.noreply.github.com>
2020-10-31 17:11:58 -0400

    Removed table functionality for simplicity

--- frontend/src/components/SubDurple.js
+++ frontend/src/components/SubDurple.js
@@ -1,14 +1,12 @@
 import React, {useState} from "react";
 import '../styles/style.css';
 import { Cards } from "./Cards";
-import { Table } from "./Table";
 import { useSubData, useSubAddress, useDurpleContext } from "../hooks/Durple";
 import { Loading } from "./Loading";
 import logo from "../assets/durple grey.png";
 
 export function SubDurple() {
   const subData = useSubData();
-  const [view, setView] = useState("cards");
   const durple = useDurpleContext();
 
   useSubAddress();
@@ -43,17 +41,9 @@ export function SubDurple() {
     <>
     <div className="container text-center">
       <h1 className="subTitle mt-4 mb-4"><b>{WelcomeString()}</b></h1>
-      <div className="btn-group mb-4" role="group" aria-label="Basic example">
-        <button type="button" className={"btn btn-sm "+(view==="cards"?"btn-secondary":"btn-outline-secondary")} onClick={() => setView("cards")}>Cards</button>
-        <button type="button" className={"btn btn-sm "+(view==="table"?"btn-secondary":"btn-outline-secondary")} onClick={() => setView("table")}>Table</button>
-      </div>
     </div>
     {subData&&subData.postCount > 0?
-      <>{
-        view === "cards"?
         <Cards posts={posts()}/>
-        :<Table posts={posts()}/>
-      }</>
       :<Logo />
     }
     </>
